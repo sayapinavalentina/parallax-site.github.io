efa2637e2bc7eca18223313808f8e92e5c908ea9
sayapinavalentina <sayapinavalentina7@gmail.com>
2023-03-28 01:37:32 +0300

    add parallax effect

---
+++ js/app.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', e => {
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
+
+//Получить высоту страницы
